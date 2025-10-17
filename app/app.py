aa11cd2d69450aea4025d54d6cd94ed4513a02b5
DiegoRigazio <diegorigazio@gmail.com>
2025-10-17 08:52:00 -0300

    fix: remove circular import (app.app <-> app.main)

--- app/app.py
+++ app/app.py
@@ -1,2 +1,2 @@
 # Si tu app está en backend/main.py con "app = FastAPI()"
-from .main import app  # ajustá el import según tu estructura
+from app.main import app  # ajustá el import según tu estructura
